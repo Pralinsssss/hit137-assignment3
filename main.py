ff399b84b8eea82e626efc2e50186283335e5ddb
Pralin Dhungana <pralin.dhungana@gmail.com>
2025-09-15 00:43:35 +0930

    Added image classification Model and neutralized emotion detector~

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
-
+'''
 pipe = pipeline("text-classification", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
 while True:
@@ -8,3 +8,15 @@ while True:
         break
     result = pipe(user_input)
     print("Result:", result)
+ '''
+
+pipe = pipeline("image-classification", model="google/vit-base-patch16-224")
+image_path = input("Enter image filename: ")
+result = pipe(image_path)
+
+best_guess = result[0]
+label = best_guess['label']
+confidence = best_guess['score'] * 100
+
+clean_output = f"{label} ({confidence:.1f}% confidence)"
+print(clean_output)
