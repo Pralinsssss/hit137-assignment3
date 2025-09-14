22cd80006c3cc73fce9ccc480d3a41112b79623f
Pralin Dhungana <pralin.dhungana@gmail.com>
2025-09-14 22:58:36 +0930

    Added working sentiment analysis model

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("Hi")
+from transformers import pipeline
+
+pipe = pipeline("text-classification", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+
+while True:
+    user_input = input("Enter your text: ")
+    if user_input.lower() == 'quit':
+        break
+    result = pipe(user_input)
+    print("Result:", result)
